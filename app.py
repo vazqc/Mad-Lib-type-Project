2cf0dad6c3b60d95b6fc13e7c3dc003b6fe09964
ripped . <vazqc752@hsd.k12.or.us>
2024-04-29 12:39:22 -0700

    Adding app file.

---
+++ app.py
@@ -0,0 +1,23 @@
+"""
+
+Rad-Rib
+By Charles Vazquez
+A Mad-lib-like program for a school project."""
+
+# imports
+
+
+# Welcome and greet user
+
+# Get user input
+
+
+
+# Build story
+
+
+
+# Display results
+
+
+# Thank user and quit program
