4e04e0fbfcc3a959758c43e82a19a64da66eb0db
ripped . <vazqc752@hsd.k12.or.us>
2024-05-29 17:53:03 -0700

    Git issue #4; retry

--- app.py
+++ app.py
@@ -64,15 +64,18 @@ sleep(4)
 
 animal = input("Enter an animal: ")
 object = input("Enter an object: ")
-adjective = input("Enter the first adjective")
+adjective = input("Enter the first adjective: ")
 place = input("Enter a place: ")
-adjective2 = input("Enter a second adjective")
+adjective2 = input("Enter a second adjective: ")
 adjective3 = input("Enter a third adjective: ")
-
+object2 = input("Enter another object: ")
+place2 = input("Enter a second place:")
 
 story = f'Once upon a time, there was a(n) {animal} who was very {adjective}. The {animal} was approached by a moving {object}. ``How do you do {animal}?`` the {object} said.'
 story2 = f'``Well, I am very {adjective} at the moment.`` said the {animal}. ``If you are feeling like it, would you like to go to {place} later this evening?`` queried the {object}. ``I would be {adjective2} to.`` said the {animal}.'
 story3 = f'``Well to be honest, we are going anyways no matter how you feel. So lets go!`` exclaimed the {object}. {animal} has never been to {place} before. It is a new experience and the {animal} is very {adjective3}.'
+story4 = f'The {object} was taken aback after stumbling into a {object2}. ``I havent seen you in forever, {object}!`` exclaimed {object2}. ``Me neither! The last time I saw you was at {place2}!`` said {object}.'
+story5 = f'``Do you think we should take your friend there?`` questioned {object2}. ``I think thats a great idea!`` the {animal} and {object} histerically said. They then went on to visit {place2}'
 
 
 
@@ -81,7 +84,9 @@ story3 = f'``Well to be honest, we are going anyways no matter how you feel. So 
 print(story)
 print(story2)
 print(story3)
-print("\The program will close in one minute.")
+print(story4)
+print(story5)
+print("The program will close in one minute.")
 sleep(60)
 clr()
 
